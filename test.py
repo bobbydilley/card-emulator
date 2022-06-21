ecbdc60f4ba7690c39136c17cbd8c90d2d17687d
Bobby Dilley <bobbydilley@users.noreply.github.com>
2022-06-21 23:03:19 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import serial
-
-s = serial.Serial("/dev/ttyUSB0", baudrate=2000000, parity=serial.PARITY_NONE, bytesize = serial.EIGHTBITS, stopbits=serial.STOPBITS_ONE)
-
-while True:
-    print(s.read(1).hex())
